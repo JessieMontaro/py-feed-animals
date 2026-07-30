dd66da99bc762619c18adb6e98fa062f39883b5d
Jessie Montaro <montaro.daedra@gmail.com>
2026-07-30 03:17:42 +0200

    Added return type annotations

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 class Animal:
 
-    def __init__(self, name: str, appetite: int, is_hungry: bool = True):
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
 
-    def print_name(self):
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self):
+    def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
@@ -19,27 +19,27 @@ class Animal:
 
 class Cat(Animal):
 
-    def __init__(self, name: str, is_hungry: bool = True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         self.name = name
         self.is_hungry = is_hungry
         self.appetite = 3
 
-    def catch_mouse(self):
+    def catch_mouse(self) -> None:
         print("The hunt began!")
 
 
 class Dog(Animal):
 
-    def __init__(self, name: str, is_hungry: bool = True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         self.name = name
         self.is_hungry = is_hungry
         self.appetite = 7
 
-    def bring_slippers(self):
+    def bring_slippers(self) -> None:
         print("The slippers delivered!")
 
 
-def feed_animals(animal_list: list):
+def feed_animals(animal_list: list) -> int:
     hunger = 0
     for animal in animal_list:
         if animal.is_hungry:
